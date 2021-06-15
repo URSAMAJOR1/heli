fa1451ab826f43b5a02a87bd8c1a03a7cc3269ef
URSAMAJOR1 <77448571+URSAMAJOR1@users.noreply.github.com>
2021-06-15 13:14:50 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,13 @@
+class Box{} 
+    var options={
+        restiution:1 ,
+
+    }
+    this.body=Bodie.rectangle(200,100,50,50)
+    World.add(world.this.body);
+    display()
+       var pos =this.body.position;
+        recMode(CENTER)
+        Fill(255)
+    rect(pos.x,pos.y,this.width,this.height)
+    
